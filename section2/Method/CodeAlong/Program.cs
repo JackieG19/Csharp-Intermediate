1ac5beb69cb73e16f88c4aceb8308de688505c65
Jackie <jacgut19@gmail.com>
2018-06-21 16:32:50 +0000

    Method

--- section2/Method/CodeAlong/Program.cs
+++ section2/Method/CodeAlong/Program.cs
@@ -4,19 +4,24 @@ namespace Methods
 {
     class program
     {
-        public int Add(param int[] number)
-        {
-           var sum = 0;
-           foreach (var number in numbers)
-           {
-               sum += number;
-           }
-           return sum;
-        }
 
         static void Main(string[] args)
         {
-            ...
+            // var number = int.Parse("abc");
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine("Conversion failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[]{1, 2, 3, 4, 5}));
         }
 
         static void UsePoint()
@@ -25,7 +30,7 @@ namespace Methods
             try
             {
                 var point = new Point(10, 20);
-                point.Move(new Point(40, 60));
+                point.Move(new Point(null);
                 Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
 
                 point.Move(100, 200);
